5b9d50764f221062274094a9eab10172b823fbf7
seyeong.kim <seyeong.kim@snplab.io>
2022-03-28 18:09:25 +0900

    use passport-github

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express, {Request, Response} from 'express';
+import passport from 'passport';
+import passportgithub from 'passport-github2';
+import dotenv from 'dotenv';
+import envpath from './util/filepath';
 import userRouter from './routes/user';
+
+dotenv.config({ path: envpath});
+const GitHubStrategy = passportgithub.Strategy;
 
 class Server {
     public app: express.Application
@@ -17,8 +24,34 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/user', userRouter);
 
-app.get('/', (req: Request, res: Response) => {
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj: any, done) => {
+    done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        callbackURL: process.env.GITHUB_CALLBACK_URL!
+    }, (accessToken: string, refreshToken: string, profile: any, done: any) => {
+        process.nextTick(() => {
+            return done(null, profile);
+        })
+    }
+));
+
+app.get('/', function(req, res){
     res.render('main');
+});
+
+app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
+    (req: Request, res: Response) => {
+        res.redirect('/');   
 })
 
 app.set('port', 3000)
