0d551fd685fd5cd87d85b8dca111e6a8f7b91e68
kim-se-yeong <kimseyeong97@gmail.com>
2022-04-02 22:52:04 +0900

    modify path

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import envpath from '../util/filepath';
+import envpath from '../util/filePath';
 dotenv.config({ path: envpath});
 
 const config = {
