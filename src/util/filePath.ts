141fab3ce102b7d03e720dc909c35cf55774eeb7
kim-se-yeong <kimseyeong97@gmail.com>
2022-04-04 18:13:53 +0900

    update path

--- src/util/filePath.ts
+++ src/util/filePath.ts
@@ -1,5 +1,5 @@
 import path from 'path';
 
-const ENV_PATH = __dirname + path.sep + '..' + path.sep + '..' + path.sep + '.env';
+const ENV_PATH = path.join(__dirname, path.sep, '..', path.sep, '..', path.sep, '.env');
 
 export default ENV_PATH;
